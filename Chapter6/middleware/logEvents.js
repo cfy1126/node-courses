73402320582d38b61964c8b3287007105dc74328
caofayou <2017954256@qq.com>
2024-12-27 17:06:03 +0800

    中间件内容

--- Chapter6/middleware/logEvents.js
+++ Chapter6/middleware/logEvents.js
@@ -11,11 +11,11 @@ const logEvents = async (message, fileName) => {
   const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
   console.log(logItem);
   try {
-    if(!fs.existsSync(path.join(__dirname, 'logs'))){
-      fsPromises.mkdir(path.join(__dirname, 'logs'));
+    if(!fs.existsSync(path.join(__dirname, '..', 'logs'))){
+      fsPromises.mkdir(path.join(__dirname, '..', 'logs'));
     }
     await fsPromises.appendFile(
-      path.join(__dirname, 'logs', fileName),
+      path.join(__dirname, '..', 'logs', fileName),
       logItem
     );
   } catch (err) {
@@ -23,4 +23,10 @@ const logEvents = async (message, fileName) => {
   }
 };
 
-module.exports = logEvents;
+const logger = (req, res, next)=> {
+  logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt');
+  console.log(`${req.method} ${req.path}`);
+  next();
+}
+
+module.exports = { logEvents, logger };
--- Chapter6/server.js
+++ Chapter6/server.js
@@ -1,7 +1,38 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents');
+const errorHandle = require('./middleware/errorHandle');
 const PORT = process.env.PORT || 3500;
+
+// 自定义日志记录器中间件
+app.use(logger);
+
+// 允许跨域请求
+const whitelist = ['https://www.google.com','http://127.0.0.1:5500', 'http://127.0.0.1:3500'];
+const corsOptions = {
+  origin: (origin, callback)=>{
+    if(whitelist.indexOf(origin) !== -1 || !origin){
+      // null: 没有错误 true: 返回来源
+      callback(null, true);
+    }else{
+      callback(new Error('Not allowed By CORS'));
+    }
+  },
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+// 解析 application/x-www-form-urlencoded 类型的请求体数据
+app.use(express.urlencoded({extended: false}));
+
+// 解析 application/json 类型的请求体数据
+app.use(express.json());
+
+// 客户端可以直接访问 public 目录中的文件，而无需通过特定的路由处理器
+app.use(express.static(path.join(__dirname, '/public')));
+
 
 app.get('^/$|/index(.html)?', (req, res) => {
   // res.sendFile('./views/index.html', {root: __dirname});
@@ -44,8 +75,21 @@ const three = (req, res, next)=> {
 
 app.get('/chain(.html)?', [one, two, three]);
 
-app.get('/*', (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+// 处理所有未匹配路由的中间件（所有方法和路径）
+app.all('*', (req, res) => {
+  res.status(404);
+  if(req.accepts('html')){
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  }else if(req.accepts('json')){
+    res.json({
+      error: "404 Not Found"
+    })
+  }else{
+    res.type('txt').send('404 Not Found');
+  }
 });
+
+// 错误处理中间件（记录错误日志）
+app.use(errorHandle);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
