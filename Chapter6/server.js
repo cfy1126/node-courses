a0c0ebb9fa869f59fafe255838d038736ebd9cea
caofayou <2017954256@qq.com>
2024-12-28 11:45:01 +0800

    路由器设置

--- Chapter6/server.js
+++ Chapter6/server.js
@@ -10,81 +10,50 @@ const PORT = process.env.PORT || 3500;
 app.use(logger);
 
 // 允许跨域请求
-const whitelist = ['https://www.google.com','http://127.0.0.1:5500', 'http://127.0.0.1:3500'];
+const whitelist = [
+  'https://www.google.com',
+  'http://127.0.0.1:5500',
+  'http://127.0.0.1:3500',
+  "extension://eggdlmopfankeonchoflhfoglaakobma"
+];
 const corsOptions = {
-  origin: (origin, callback)=>{
-    if(whitelist.indexOf(origin) !== -1 || !origin){
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
       // null: 没有错误 true: 返回来源
       callback(null, true);
-    }else{
+    } else {
       callback(new Error('Not allowed By CORS'));
     }
   },
-  optionsSuccessStatus: 200
-}
+  optionsSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
 
 // 解析 application/x-www-form-urlencoded 类型的请求体数据
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 // 解析 application/json 类型的请求体数据
 app.use(express.json());
 
 // 客户端可以直接访问 public 目录中的文件，而无需通过特定的路由处理器
-app.use(express.static(path.join(__dirname, '/public')));
+app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
-
-app.get('^/$|/index(.html)?', (req, res) => {
-  // res.sendFile('./views/index.html', {root: __dirname});
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-
-app.get('/new-page(.html)?', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-
-app.get('/old-page(.html)?', (req, res) => {
-  res.redirect(301, '/new-page.html'); // 302 by default
-});
-
-app.get(
-  '/hello(.html)?',
-  (req, res, next) => {
-    console.log('attempted to load hello.html');
-    next();
-  },
-  (req, res) => {
-    res.send('hello hello hello');
-  }
-);
-
-const one = (req, res, next)=> {
-  console.log('one');
-  next();
-}
-
-const two = (req, res, next)=> {
-  console.log('two');
-  next();
-}
-
-const three = (req, res, next)=> {
-  console.log('three');
-  res.send('Finished!');
-}
-
-app.get('/chain(.html)?', [one, two, three]);
+// 路由器
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
 
 // 处理所有未匹配路由的中间件（所有方法和路径）
 app.all('*', (req, res) => {
   res.status(404);
-  if(req.accepts('html')){
+  if (req.accepts('html')) {
     res.sendFile(path.join(__dirname, 'views', '404.html'));
-  }else if(req.accepts('json')){
+  } else if (req.accepts('json')) {
     res.json({
-      error: "404 Not Found"
-    })
-  }else{
+      error: '404 Not Found',
+    });
+  } else {
     res.type('txt').send('404 Not Found');
   }
 });
